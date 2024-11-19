d54700624ce3de10d079b92df15f3e92beae6593
RHODRIGO LEANDRO BARROS RODRIGUES <rhodrigo.rodrigues@a.ficr.edu.br>
2024-11-19 09:00:19 -0300

    primeiro commit

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+const App = () => {
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleInputChange = (event) => {
+    setName(event.target.value);
+  };
+
+  const handleButtonClick = () => {
+    setMessage(`Olá, ${name}!! Seja bem-vindo(a)!`);
+  };
+
+  return (
+    <div id="main">
+      <h1>Insira seu nome</h1>
+      <div id="box">
+      <input  
+        type="text"
+        value={name}
+        onChange={handleInputChange}
+        placeholder="Digite seu nome"
+      />
+
+      <button
+        onClick={handleButtonClick}
+      >
+        Enviar
+      </button>
+      </div>
+      {message && (
+        <p>{message}</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
